b3cdf028151250e58fc6ebc7fe9bfb9809de9c26
boaz1e <boazy@my.hit.ac.il>
2024-02-02 17:53:54 +0200

    Improve readibility by moving code to separate files

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,23 @@
+# main.py
 from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker, relationship
-from pydantic import BaseModel
-from typing import List
-from datetime import datetime
-from decouple import config
+from sqlalchemy.orm import Session
+from models import Base, Task, TaskModel
+from database import get_db, engine
 
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
-DB_NAME = config('DB_NAME')
 
-MYSQL_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-app = FastAPI()
-
-# MySQL Database Configuration
-engine = create_engine(MYSQL_DATABASE_URL, pool_pre_ping=True)
-Base = declarative_base()
-
-class TaskModel(Base):
-    __tablename__ = "tasks"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), index=True)  
-    description = Column(String(1024))   
-    status = Column(String(50))  
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-# Create tables in the database
 Base.metadata.create_all(bind=engine)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-class Task(BaseModel):
-    title: str
-    description: str
-    status: str
+app = FastAPI()
 
 @app.post("/tasks/", response_model=Task)
 def create_task(task: Task, db: Session = Depends(get_db)):
-    db_task = TaskModel(**task.dict())
+    db_task = TaskModel(**task.model_dump())
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
     return task
 
-@app.get("/tasks/", response_model=List[Task])
+@app.get("/tasks/", response_model=list[Task])
 def read_tasks(db: Session = Depends(get_db)):
     return db.query(TaskModel).all()
 
@@ -83,4 +47,3 @@ def delete_task(task_id: int, db: Session = Depends(get_db)):
         db.commit()
         return db_task
     raise HTTPException(status_code=404, detail="Task not found")
-
