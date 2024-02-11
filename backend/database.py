60c396c8a10d116f3095c6024f6fbdca50440444
boaz1e <boazy@my.hit.ac.il>
2024-02-11 15:23:12 +0200

    Docker test

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
-DB_NAME = config('DB_NAME')
-
-MYSQL_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+MYSQL_DATABASE_URL = f"mysql+mysqlconnector://root:12345@127.0.0.1:3306/task-management-db"
 
 engine = create_engine(MYSQL_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
