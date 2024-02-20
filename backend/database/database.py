1269769ac1a2b2b01acd1ed4e8f72cb9808d20b1
boaz1e <boazy@my.hit.ac.il>
2024-02-20 17:38:52 +0200

    Change database.py configurations

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,13 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
-DB_NAME = config('DB_NAME')
 
-MYSQL_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+MYSQL_DATABASE_URL = "mysql+mysqlconnector://user:password@mysql-db:3306/db"
 
 engine = create_engine(MYSQL_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
